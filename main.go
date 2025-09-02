1d1f5146095b74f9810ac14d8751de2ed8819e03
Darshan-Baslani <darshan.baslani4@gmail.com>
2025-09-02 10:19:58 +0530

    basic listener and stdout which reads 8byte

---
+++ main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"log"
+	"io"
+	"net"
+)
+
+func getLinesChannel(file io.ReadCloser) <-chan string {
+	data := make(chan string)
+	go func() {
+		str := ""
+		for {
+			stream := make([]byte, 8)
+			_, err := file.Read(stream)
+			if err == io.EOF {
+				close(data)
+				break
+			}
+			if i := bytes.IndexByte(stream, byte('\n')); i != -1 {
+				str += string(stream[:i])
+				// fmt.Printf("read: %s\n", str)
+				stream = stream[i+1:]
+				data <- str
+				str = ""
+			}
+			str += string(stream)
+
+			// fmt.Printf("read: %s\n", string(stream))
+		}
+	}()
+	return data
+}
+
+func main() {
+	listener, err := net.Listen("tcp", ":42069")
+	if err != nil {
+		log.Fatal("error listening to port :42069")
+	}
+	defer listener.Close()
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Fatal("error aceepting request")
+		}
+		for data := range getLinesChannel(conn)	{
+			fmt.Printf("read: %s\n", data)
+		}
+	}
+
+}
