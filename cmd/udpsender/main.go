c159cd08c2f5e615bbd13c02413553d7776461c3
Darshan-Baslani <darshan.baslani4@gmail.com>
2025-09-03 13:17:51 +0530

    add: udplistener, modifier: file structure

---
+++ cmd/udpsender/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"bufio"
+	"os"
+	"log"
+)
+
+func main() {
+	raddr, err := net.ResolveUDPAddr("udp", ":42069")
+	if err != nil {
+		log.Fatal("error resolving udp addr at :42069")
+	}
+
+	conn, err := net.DialUDP("udp", nil, raddr)
+	defer conn.Close()
+	if err != nil {
+		log.Fatal("error dialling up udp addr at :42069")
+	}
+
+	reader := bufio.NewReader(os.Stdin)
+	for {
+		fmt.Printf(">")
+		input, err := reader.ReadString('\n')
+		if err != nil {
+			log.Fatal("error reading string")
+		}
+		
+		_, err = conn.Write([]byte(input))
+		if err != nil {
+			log.Fatal("error writing to connection")
+		}
+	}
+}
